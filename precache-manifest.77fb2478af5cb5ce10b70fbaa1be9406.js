d6e517a054af776bcf425990d8ff821887b9c5cf
Andriy <powerlviv@ukr.net>
2020-09-03 18:43:40 +0300

    Updates

---
+++ precache-manifest.77fb2478af5cb5ce10b70fbaa1be9406.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3955b9ed99ac4f9da6f19943d2e82d",
+    "url": "/reactFirst/index.html"
+  },
+  {
+    "revision": "aa4aeb63e5c41e9112e4",
+    "url": "/reactFirst/static/css/main.e42977d5.chunk.css"
+  },
+  {
+    "revision": "0a8fa4c790d0518f9d06",
+    "url": "/reactFirst/static/js/2.c656c403.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactFirst/static/js/2.c656c403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4aeb63e5c41e9112e4",
+    "url": "/reactFirst/static/js/main.496a3e75.chunk.js"
+  },
+  {
+    "revision": "56b3fa901b1b6a68e47f",
+    "url": "/reactFirst/static/js/runtime-main.2bcc09da.js"
+  },
+  {
+    "revision": "371f610ea50e8030a3fa6fffff9ce72a",
+    "url": "/reactFirst/static/media/arrow.371f610e.svg"
+  },
+  {
+    "revision": "8d3e51c7c10bb2d84c72b4682fa605b0",
+    "url": "/reactFirst/static/media/comment.8d3e51c7.svg"
+  },
+  {
+    "revision": "c2e1643f330325fb8920055d0df3d36e",
+    "url": "/reactFirst/static/media/like.c2e1643f.svg"
+  },
+  {
+    "revision": "501c6325ded11fd509ea06f807af1107",
+    "url": "/reactFirst/static/media/repost.501c6325.svg"
+  },
+  {
+    "revision": "499187f3b67e6683709b237e095b7e31",
+    "url": "/reactFirst/static/media/share.499187f3.svg"
+  },
+  {
+    "revision": "2019cadf6bdc5d95f29a232dff10f6ea",
+    "url": "/reactFirst/static/media/verified.2019cadf.svg"
+  }
+]);
